e87c172d5248479d35ce34d0a680b3c75c8626dc
Petr Kratina <petr.kratina@gmail.com>
2014-02-09 02:15:55 +0100

    removed unused fields from config

--- config.h
+++ config.h
@@ -24,26 +24,6 @@
 128 - 1V
 */
 
-/* DISPLAY */
-
-#define LCD_RS 9
-#define LCD_ENABLE 8
-#define LCD_D4 5
-#define LCD_D5 4
-#define LCD_D6 3
-#define LCD_D7 2
-#define LCD_ROWS 2
-#define LCD_COLS 16
-
-/* SOUND */
-#define SOUND_PIN 10
-#define SOUND_ENABLED true
-
-/* NES Controller */
-#define NES_LATCH A1
-#define NES_CLOCK A2
-#define NES_SERIAL A0
-
 /* Registers */
 #define REG_DATA 11
 #define REG_LATCH 12
